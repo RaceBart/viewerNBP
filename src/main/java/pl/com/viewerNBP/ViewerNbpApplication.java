80ad44ca67aff82fc4a9d2593c56b226210802ce
Bartosz Łączyński <blaczynski@vigo.com.pl>
2018-07-10 20:34:08 +0200

    First commit

---
+++ src/main/java/pl/com/viewerNBP/ViewerNbpApplication.java
@@ -0,0 +1,12 @@
+package pl.com.viewerNBP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewerNbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewerNbpApplication.class, args);
+	}
+}
