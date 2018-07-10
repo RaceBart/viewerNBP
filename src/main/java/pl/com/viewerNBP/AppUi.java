86e03d7a884400a6c90ed1e02e6ede98833965ff
Bartosz Łączyński <blaczynski@vigo.com.pl>
2018-07-10 22:24:47 +0200

    test REST

--- src/main/java/pl/com/viewerNBP/AppUi.java
+++ src/main/java/pl/com/viewerNBP/AppUi.java
@@ -2,8 +2,18 @@ package pl.com.viewerNBP;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.HttpGet;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+
+
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -12,6 +22,7 @@ import com.vaadin.ui.VerticalLayout;
 public class AppUi extends UI{
 	
 	Label testLabel = new Label("test");
+	Button test = new Button("test");
 	VerticalLayout layout = new VerticalLayout() ;
 	
 	@Autowired
@@ -20,7 +31,23 @@ public class AppUi extends UI{
 
 	@Override
 	protected void init(VaadinRequest request) {
-		layout.addComponent(testLabel);
+		
+		test.addClickListener(c->{
+			HttpResponse response = null;
+			String connectURL = "http://api.nbp.pl/api/exchangerates/rates/A/USD/";
+			HttpClient httpClient = HttpClientBuilder.create().build();
+			HttpGet requestGet = new HttpGet(connectURL);
+			try {
+				response = httpClient.execute(requestGet);
+				testLabel.setValue(response.toString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		});
+
+		layout.addComponents(test,testLabel);
 		setContent(layout);		
 	}
 
