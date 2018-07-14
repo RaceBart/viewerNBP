e0940f64a923a683f5fc358c61622c1a1ef2032b
Bartosz Łączyński <blaczynski@vigo.com.pl>
2018-07-14 20:57:53 +0200

    ui test 11

--- src/main/java/pl/com/viewerNBP/ui/AppUi.java
+++ src/main/java/pl/com/viewerNBP/ui/AppUi.java
@@ -1,11 +1,12 @@
 package pl.com.viewerNBP.ui;
 
-
-
-
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -27,38 +28,49 @@ import com.vaadin.ui.VerticalLayout;
 
 @SpringUI
 public class AppUi extends UI {
-	
+
 	@Autowired
 	CurrenciesModelRepo modelRepo;
 
 	private VerticalLayout root;
 	private VerticalLayout currencyList;
-	private Button addBt = new Button("Add",VaadinIcons.PLUS);
-	private Button clearBt = new Button("Clear",VaadinIcons.CLOSE);
-	private Button drawBt = new Button("Draw",VaadinIcons.PENCIL);
-	private Button databaseValueSettBt = new Button("DB Settings",VaadinIcons.COG_O);
-	private Button predictBt = new Button("Predict",VaadinIcons.CLOCK);
-	private ComboBox<String> currencyChoose=new ComboBox<>();
+	private Button addBt = new Button("Add", VaadinIcons.PLUS);
+	private Button clearBt = new Button("Clear", VaadinIcons.CLOSE);
+	private Button drawBt = new Button("Draw", VaadinIcons.PENCIL);
+	private Button databaseValueSettBt = new Button("DB Settings", VaadinIcons.COG_O);
+	private Button predictBt = new Button("Predict", VaadinIcons.CLOCK);
+	private ComboBox<String> currencyChoose = new ComboBox<>();
 	private ListSelect<String> sample = new ListSelect<>("Select an option");
 	private DateField startDate = new DateField();
-    private DateField endDate =  new DateField();	
-    private List<String> selectedCurrencies = new LinkedList();
-    private List<String> downloadedCurrencies = new LinkedList();
-    private List<List<CurrenciesModel>> dataToDraw = new LinkedList();
+	private DateField endDate = new DateField();
+	private List<String> selectedCurrencies = new LinkedList();
+	private List<String> downloadedCurrencies = new LinkedList();
+	private List<List<CurrenciesModel>> dataToDraw = new LinkedList();
 	private Comparator<CurrenciesModel> dataComparator = new Comparator<CurrenciesModel>() {
-	      public int compare(CurrenciesModel o1, CurrenciesModel o2) {
-	          return o1.getCurrency_date().compareTo(o2.getCurrency_date());
-	          }};
+		public int compare(CurrenciesModel o1, CurrenciesModel o2) {
+			return o1.getCurrency_date().compareTo(o2.getCurrency_date());
+		}
+	};
 
-    
 	private NbpApiSender nbpSender = new NbpApiSender();
 	private Chart chart;
-    
+
 	@Override
 	protected void init(VaadinRequest request) {
-		
-		
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			// Date date = sdf.parse("2018-07-01");
+			List<CurrenciesModel> sampleList = new LinkedList();
+			sampleList.add(new CurrenciesModel(sdf.parse("2018-07-01"), "test", 1.1));
+			sampleList.add(new CurrenciesModel(sdf.parse("2018-07-02"), "test", 2.1));
+			sampleList.add(new CurrenciesModel(sdf.parse("2018-07-03"), "test", 1.1));
+			dataToDraw.add(sampleList);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		root = new VerticalLayout();
 		setContent(root);
 		addButtonsLayout();
@@ -69,27 +81,25 @@ public class AppUi extends UI {
 
 	}
 
-
 	private void addChartLayout() {
-//		List<CurrenciesModel> cur1 = modelRepo.findByCurrencyname("euro");
-//		
-//	    Collections.sort(cur1, new Comparator<CurrenciesModel>() {
-//	        public int compare(CurrenciesModel o1, CurrenciesModel o2) {
-//	            return o1.getCurrency_date().compareTo(o2.getCurrency_date());
-//	            }
-//	    });
+		// List<CurrenciesModel> cur1 = modelRepo.findByCurrencyname("euro");
+		//
+		// Collections.sort(cur1, new Comparator<CurrenciesModel>() {
+		// public int compare(CurrenciesModel o1, CurrenciesModel o2) {
+		// return o1.getCurrency_date().compareTo(o2.getCurrency_date());
+		// }
+		// });
 
-//		chart = new Chart();
-//		Component chartComp = chart.chartLine(cur1);
-//		chartComp.setSizeFull();
-//		HorizontalLayout chartLayout = new HorizontalLayout();
-//
-//		chartLayout.addComponentsAndExpand(chartComp);
-//		chartLayout.setSizeFull();
-//		root.addComponent(chartLayout);
+		// chart = new Chart();
+		// Component chartComp = chart.chartLine(cur1);
+		// chartComp.setSizeFull();
+		// HorizontalLayout chartLayout = new HorizontalLayout();
+		//
+		// chartLayout.addComponentsAndExpand(chartComp);
+		// chartLayout.setSizeFull();
+		// root.addComponent(chartLayout);
 
 	}
-
 
 	private void addButtonsLayout() {
 		HorizontalLayout dataChooseLayout = new HorizontalLayout();
@@ -101,85 +111,80 @@ public class AppUi extends UI {
 		startDate.setDateFormat("yyyy-MM-dd");
 		endDate.setDateFormat("yyyy-MM-dd");
 		endDate.setValue(LocalDate.now());
-		dataChooseLayout.addComponents(sample,addBt,clearBt,drawBt,currencyChoose,startDate,endDate,databaseValueSettBt,predictBt);
-		root.addComponent(dataChooseLayout);		
+		dataChooseLayout.addComponents(sample, addBt, clearBt, drawBt, currencyChoose, startDate, endDate,
+				databaseValueSettBt, predictBt);
+		root.addComponent(dataChooseLayout);
 	}
 
-
 	private void setupButtonsBehaviour() {
-		
 
-		
-		addBt.addClickListener(c->{
-			if(!selectedCurrencies.contains(currencyChoose.getValue())) {
+		addBt.addClickListener(c -> {
+			if (!selectedCurrencies.contains(currencyChoose.getValue())) {
 				selectedCurrencies.add(currencyChoose.getValue());
-//				selectedCurrenciesCombo.setItems(selectedCurrencies);
-			}else {
+				// selectedCurrenciesCombo.setItems(selectedCurrencies);
+			} else {
 				Notification.show("Choose new currency");
 			}
 		});
-		
-		clearBt.addClickListener(c->{
+
+		clearBt.addClickListener(c -> {
 			Notification.show(nbpSender.getBaseURL());
 		});
-		
-		drawBt.addClickListener(c->{
-//			addChartLayout();
-//			selectedCurrenciesCombo.setItems(selectedCurrencies);
-			
-			if(!selectedCurrencies.isEmpty()) {
+
+		drawBt.addClickListener(c -> {
+			// addChartLayout();
+			// selectedCurrenciesCombo.setItems(selectedCurrencies);
+
+			if (!selectedCurrencies.isEmpty()) {
 				dataToDraw.clear();
-				selectedCurrencies.stream().forEach(s->{
+				selectedCurrencies.stream().forEach(s -> {
 					dataToDraw.add(modelRepo.findByCurrencyname(s));
 					downloadedCurrencies.add(s);
 				});
-				
-				dataToDraw.stream().forEach(d->{
+
+				dataToDraw.stream().forEach(d -> {
 					Collections.sort(d, dataComparator);
 				});
-				HorizontalLayout chartLayout = new HorizontalLayout();
+//				HorizontalLayout chartLayout = new HorizontalLayout();
 				chart = new Chart();
 				Component chartComp = chart.chartLine(dataToDraw);
 				chartComp.setSizeFull();
-				chartLayout.addComponentsAndExpand(chartComp);
-				chartLayout.setSizeFull();
-				root.addComponent(chartLayout);
+//				chartLayout.addComponentsAndExpand(chartComp);
+//				chartLayout.setSizeFull();
+				root.addComponent(chartComp);
 
-				
-				Notification.show(String.valueOf(selectedCurrencies.size())+ "  " + String.valueOf(downloadedCurrencies.size()));
+				Notification.show(
+						String.valueOf(selectedCurrencies.size()) + "  " + String.valueOf(downloadedCurrencies.size()));
 
-				
-			}else {
+			} else {
 				Notification.show("Choose some currency to show");
 			}
 		});
-		
-		databaseValueSettBt.addClickListener(s->{
-			SettingsWindow settingsWindow = new SettingsWindow("Database Value Settings",modelRepo);
-			settingsWindow.setHeight(50, Unit.PERCENTAGE);
-			settingsWindow.setWidth(40, Unit.PERCENTAGE);
+
+		databaseValueSettBt.addClickListener(s -> {
+			SettingsWindow settingsWindow = new SettingsWindow("Database Value Settings", modelRepo);
+			settingsWindow.setHeight(20, Unit.PERCENTAGE);
+			settingsWindow.setWidth(30, Unit.PERCENTAGE);
 			settingsWindow.center();
 			settingsWindow.setDraggable(true);
 			settingsWindow.setModal(true);
 			UI.getCurrent().addWindow(settingsWindow);
 		});
 
-		predictBt.addClickListener(c->{
-			
+		predictBt.addClickListener(c -> {
+
 		});
-		
+
 	}
-	
+
 	private void drawChart() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void getVaules() {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 }
